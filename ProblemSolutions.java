70c11c8a22c5c0252717a1953f34910b0885502a
megluc <140560593+megluc@users.noreply.github.com>
2024-11-06 19:57:40 -0600

    Update ProblemSolutions.java

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -1,82 +1,107 @@
-/******************************************************************
- *
- *   YOUR NAME / SECTION NUMBER
- *
- *   This java file contains the problem solutions of isSubSet, findKthLargest,
- *   and sort2Arrays methods. You should utilize the Java Collection Framework for
- *   these methods.
- *
- ********************************************************************/
+
+    /******************************************************************
+     *
+     *   Meghan Garcia / SECTION 003
+     *
+     *   This java file contains the problem solutions of isSubSet, findKthLargest,
+     *   and sort2Arrays methods. You should utilize the Java Collection Framework for
+     *   these methods.
+     *
+     ********************************************************************/
 
 import java.util.*;
 
-class ProblemSolutions {
+    class ProblemSolutions {
 
-    /**
-     * Method: isSubset()
-     *
-     * Given two arrays of integers, A and B, return whether
-     * array B is a subset if array A. Example:
-     *      Input: [1,50,55,80,90], [55,90]
-     *      Output: true
-     *      Input: [1,50,55,80,90], [55,90, 99]
-     *      Output: false
-     *
-     * The solution time complexity must NOT be worse than O(n).
-     * For the solution, use a Hash Table.
-     *
-     * @param list1 - Input array A
-     * @param list2 - input array B
-     * @return      - returns boolean value B is a subset of A.
-     */
+        /**
+         * Method: isSubset()
+         *
+         * Given two arrays of integers, A and B, return whether
+         * array B is a subset if array A. Example:
+         *      Input: [1,50,55,80,90], [55,90]
+         *      Output: true
+         *      Input: [1,50,55,80,90], [55,90, 99]
+         *      Output: false
+         *
+         * The solution time complexity must NOT be worse than O(n).
+         * For the solution, use a Hash Table.
+         *
+         * @param list1 - Input array A
+         * @param list2 - input array B
+         * @return      - returns boolean value B is a subset of A.
+         */
 
-    public boolean isSubset(int list1[], int list2[]) {
+        public boolean isSubset(int list1[], int list2[]) {
 
-        // ADD YOU CODE HERE -- DON'T FORGET TO ADD YOR NAME AT TOP OF FILE
+            HashSet<Integer> set = new HashSet<>();
 
-        return false;
+            for(int num : list1) {
+                set.add(num);
+
+            }
+
+            for(int num : list2){
+                if(!set.contains(num)){
+                    return false;
+
+                }
+            }
+            // ADD YOU CODE HERE -- DON'T FORGET TO ADD YOR NAME AT TOP OF FILE
+
+            return true;
+        }
+
+
+        /**
+         * Method: findKthLargest
+         *
+         * Given an Array A and integer K, return the k-th maximum element in the array.
+         * Example:
+         *      Input: [1,7,3,10,34,5,8], 4
+         *      Output: 7
+         *
+         * @param array - Array of integers
+         * @param k     - the kth maximum element
+         * @return      - the value in the array which is the kth maximum value
+         */
+
+        public int findKthLargest(int[] array, int k) {
+
+            Arrays.sort(array);
+            return array[array.length - k];
+
+            // ADD YOUR CODE HERE
+
+
+        }
+
+
+        /**
+         * Method: sort2Arrays
+         *
+         * Given two arrays A and B with n and m integers respectively, return
+         * a single array of all the elements in A and B in sorted order. Example:
+         *      Input: [4,1,5], [3,2]
+         *      Output: 1 2 3 4 5
+         *
+         * @param array1    - Input array 1
+         * @param array2    - Input array 2
+         * @return          - Sorted array with all elements in A and B.
+         */
+
+        public int[] sort2Arrays(int[] array1, int[] array2) {
+            int[] mergedArray = new int[array1.length + array2.length];
+
+            System.arraycopy(array1, 0, mergedArray, 0, array1.length);
+            System.arraycopy(array2, 0, mergedArray, 0, array2.length);
+
+            Arrays.sort(mergedArray);
+
+
+            // ADD YOU CODE HERE
+
+            return mergedArray;
+        }
+
     }
-
-
-    /**
-     * Method: findKthLargest
-     *
-     * Given an Array A and integer K, return the k-th maximum element in the array.
-     * Example:
-     *      Input: [1,7,3,10,34,5,8], 4
-     *      Output: 7
-     *
-     * @param array - Array of integers
-     * @param k     - the kth maximum element
-     * @return      - the value in the array which is the kth maximum value
-     */
-
-    public int findKthLargest(int[] array, int k) {
-
-        // ADD YOUR CODE HERE
-
-        return 0;
-    }
-
-
-    /**
-     * Method: sort2Arrays
-     *
-     * Given two arrays A and B with n and m integers respectively, return
-     * a single array of all the elements in A and B in sorted order. Example:
-     *      Input: [4,1,5], [3,2]
-     *      Output: 1 2 3 4 5
-     *
-     * @param array1    - Input array 1
-     * @param array2    - Input array 2
-     * @return          - Sorted array with all elements in A and B.
-     */
-
-    public int[] sort2Arrays(int[] array1, int[] array2) {
-
-        // ADD YOU CODE HERE
-
-        return null;
-    }
-
-}
+     
